660dfec8064f3c9fbc162044efb87afb2cc5cdfc
Yurii Zhuk <iamyuriizhuk@gmail.com>
2021-04-24 19:30:20 -0400

    Turned on error reporting and require autoload.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//Turn on error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+//Require autoload file
+require_once ('vendor/autoload.php');
+
