934d9492063b079ad3092855b29a4d7801bbc8da
Yurii Zhuk <iamyuriizhuk@gmail.com>
2021-04-24 19:31:38 -0400

    Install, define routes, and run fat free

--- index.php
+++ index.php
@@ -8,3 +8,16 @@ error_reporting(E_ALL);
 //Require autoload file
 require_once ('vendor/autoload.php');
 
+// Instance Fat-Free
+$f3 = Base::instance();
+
+// Define routes
+$f3->route('GET /', function(){
+    // Display the home page
+    $view = new Template();
+    echo $view->render('views/home.html');
+
+});
+
+// Run Fat-Free
+$f3->run();
